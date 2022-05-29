a6b0d2fa6e322497dba7f18355352a2b6a3c3216
Aryse Tansy <medictansy@gmail.com>
2022-05-29 13:31:16 -0400

    Ready To Push

---
+++ README.md
@@ -0,0 +1,38 @@
+# PROJECT
+
+- Create a Opinion platform
+- Random topic generated daily
+- User will be able to type a single opinion on the topic
+- That will be rewritten once a new topic appears
+- The name will be available dynamically
+- All opinions will be rendered in the opinion page
+- Root will require registration and login
+- MongoDB-Atlas for Database
+- Express Session for cookies and session
+- Passport for Encryption, hash, salting
+- Only those authenticated will see posts page
+
+---
+
+# PLAN OF ACTION:
+
+# STEP ONE: ROOT(HOME)
+
+- At root, a welcome, log in button, and register button will be display.
+- Register button will redirect to Register Route
+- Log in button will redirect to Login Route
+
+# REGISTER
+
+- If successful, redirect to Posts
+- Else, Redirect to Register
+
+# LOG IN
+
+- If Successfull, redirect to Posts
+- Else, redirect to Log In
+
+# SESSION OVER (Closing Browsing Session or LOGOUT)
+
+- If Session Over Redirect to Log In
+- If LOGOUT Redirect to ROOT (Home)
---
+++ index.js
@@ -0,0 +1,158 @@
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const app = express();
+const passport = require("passport");
+const session = require("express-session");
+const passportLocalMongoose = require("passport-local-mongoose");
+
+// API:
+const NewsAPI = require("newsapi");
+const newsapi = new NewsAPI(`${process.env.NEWS_API_KEY}`);
+
+let topic = "TEST TOPIC";
+
+const getTopic = () => {
+  newsapi.v2
+    .topHeadlines({
+      q: "latest",
+      category: "politics",
+      language: "en",
+      country: "us",
+    })
+    .then((response) => {
+      topic = response.articles[0].title;
+    });
+  console.log("getting topic");
+};
+
+// TOPIC:
+getTopic();
+setInterval(getTopic, 1000 * 60 * 60 * 24);
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.use(
+  session({
+    secret: `${process.env.SECRET}`,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+// DB:
+mongoose.connect(
+  `mongodb+srv://${process.env.ATLAS_USER}:${process.env.ATLAS_PASS}@cluster0.k7bhq.mongodb.net/userDB`
+);
+
+const userSchema = new mongoose.Schema({
+  alias: String,
+  username: String,
+  password: String,
+  post: String,
+});
+
+userSchema.plugin(passportLocalMongoose);
+
+const User = new mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+// Gets:
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+
+app.get("/posts", (req, res) => {
+  if (req.isAuthenticated()) {
+    User.find({ post: { $ne: null } }, (err, foundUsers) => {
+      if (err) {
+        console.log(err);
+      } else {
+        if (foundUsers) {
+          res.render("posts", {
+            topic: topic,
+            currentUser: req.user.alias,
+            usersWithPost: foundUsers,
+          });
+        }
+      }
+    });
+  } else {
+    res.redirect("/login");
+  }
+});
+
+// POSTS:
+app.post("/login", (req, res) => {
+  const user = new User({
+    username: req.body.username,
+    password: req.body.password,
+  });
+
+  req.logIn(user, (err) => {
+    if (err) {
+      res.redirect("/login");
+    } else {
+      passport.authenticate("local")(req, res, () => {
+        res.redirect("/posts");
+      });
+    }
+  });
+});
+
+app.post("/register", (req, res) => {
+  User.register(
+    { alias: req.body.alias, username: req.body.username },
+    req.body.password,
+    (err, user) => {
+      if (err) {
+        console.log(err);
+        res.redirect("/register");
+      } else {
+        currentUser = user;
+        passport.authenticate("local")(req, res, () => {
+          res.redirect("/posts");
+        });
+      }
+    }
+  );
+});
+
+app.post("/logout", (req, res) => {
+  req.logOut();
+  res.redirect("/");
+});
+
+app.post("/createPost", (req, res) => {
+  let newPost = req.body.postInput;
+  User.findById(req.user.id, (err, foundUser) => {
+    if (err) {
+      console.log(err);
+    } else {
+      if (foundUser) {
+        foundUser.post = newPost;
+        foundUser.save(() => {
+          res.redirect("/posts");
+        });
+      }
+    }
+  });
+});
+
+// Server:
+app.listen(process.env.PORT || 3000, () => console.log("Spinning"));
